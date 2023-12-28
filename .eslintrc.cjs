b7864492bed462d6e3d9ae617bf2085a9935a6e9
Simon Krüger <krueger.simon2000@gmail.com>
2023-12-28 13:40:24 +0100

    added airbnb config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,14 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: ['airbnb-base', 'airbnb-typescript/base', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
+    parserOptions: {
+        project: "./tsconfig.json"
+    },
+    rules: {
+        "no-await-in-loop": 0,
+        "no-restricted-syntax": 0,
+    },
     root: true,
 };
