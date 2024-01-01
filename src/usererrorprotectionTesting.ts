40d66c83e3f39b3ec3517580b92e9cebe78c0c00
Simon Krüger <krueger.simon2000@gmail.com>
2024-01-01 23:42:00 +0100

    usererrorprotectionTesting.ts

--- src/usererrorprotectionTesting.ts
+++ src/usererrorprotectionTesting.ts
@@ -2,11 +2,21 @@
 import { PrismaClient } from '@prisma/client';
 import {DataSource} from "typeorm";
 import {Assignment, Course, Instructor, Participant} from "./typeormInit";
+import {instructor} from "./sequelizeInit";
 
 const prisma = new PrismaClient({
   log: ['query', 'info', 'warn', 'error'],
 });
-// Create
+
+
+/*
+Eingebaute Fehler:
+firstName ist vom Typ String
+lastName ist vom Typ String
+instructor-pk existiert nicht.
+Erkannte Fehler:
+Alle
+ */
 await prisma.instructor.create({
   data: {
       firstName: 22,
@@ -14,30 +24,104 @@ await prisma.instructor.create({
       instructor-pk: 1
   },
 });
-// Read
+
+/*
+Eingebaute Fehler:
+firstName ist vom Typ String
+Surname existiert nicht
+Erkannte Fehler:
+Alle
+ */
 await prisma.instructor.findMany({
     where: {
         firstName: 2
     }, select: {
-        firstname: true
+        surname: true
     }
 })
-// update
+
+/*
+Eingebaute Fehler:
+firstName darf keine Number sein
+Erkannte Fehler:
+Alle
+ZUSATZ: Prisma sagt, dass firstName keine unique Auswahl trifft.
+ */
 await prisma.instructor.update({
     where: {
         firstName: ""
     },
+    data: {
+        firstName: 1
+    }
 })
-// delete
+
+/*
+Eingebaute Fehler:
+firstName ist vom Typ String
+Erkannte Fehler:
+Keine
+ */
 await prisma.instructor.delete({
     where: {
         firstName: ""
     },
 })
-/*
-Prisma deckt alle eingebauten Fehler auf. Diese Fehler resultieren daraus, dass die Statements nicht zu den in der Datenbank definierten Typen passen.
- */
+
 // sequelize
+/*
+Eingebaute Fehler:
+firstName ist vom Typ String
+lastName ist vom Typ String
+instructor-pk existiert nicht.
+Erkannte Fehler:
+Alle
+ */
+await instructor.create({
+    firstName: 22,
+    lastName: true,
+    instructor-pk: 1
+})
+
+/*
+Eingebaute Fehler:
+firstName ist vom Typ String
+Surname existiert nicht
+Erkannte Fehler:
+Keiner
+ */
+await instructor.findAll({
+    where: {
+        firstName: 2
+    }, include: ["surname"]
+})
+
+/*
+Eingebaute Fehler:
+firstName ist vom Typ String
+Surname existiert nicht
+Erkannte Fehler:
+Keine
+ */
+await instructor.update({ firstName: 1 }, {
+    where: {
+        lastName: ""
+    }
+});
+
+/*
+Eingebaute Fehler:
+firstName ist vom Typ String
+Erkannte Fehler:
+Keine
+ */
+await instructor.destroy({
+    where: {
+        firstName: 2
+    }
+})
+
+
 // TypeORM
 const AppDataSource = new DataSource({
     type: 'sqlite',
@@ -48,26 +132,52 @@ const AppDataSource = new DataSource({
 });
 const typeOrmDatasource = await AppDataSource.initialize();
 const instructorRepo = typeOrmDatasource.getRepository(Instructor);
-
+/*
+Eingebaute Fehler:
+firstName ist vom Typ String
+lastName ist vom Typ String
+instructor-pk existiert nicht.
+Erkannte Fehler:
+Alle
+ */
 await instructorRepo.save(instructorRepo.create({
     firstName: 22,
     lastName: true,
     instructor-pk: 1
 }));
 
+/*
+Eingebaute Fehler:
+firstName ist vom Typ String
+Surname existiert nicht
+Erkannte Fehler:
+Alle
+ */
 await instructorRepo.find({
     where: {
         firstName: 2
     }, select: {
-        firstname: true
+        surname: true
     }
 });
 
-await instructorRepo.update();
-
-await instructorRepo.delete({ firstName: 2 });
 /*
-Prisma deckt alle eingebauten Fehler auf. Diese Fehler resultieren daraus, dass die Statements nicht zu den in der Datenbank definierten Typen passen.
+Eingebaute Fehler:
+firstName ist vom Typ String
+Surname existiert nicht
+Erkannte Fehler:
+Alle
  */
+await instructorRepo.update({firstName: ""}, {firstName: 1});
+
+
+/*
+Eingebaute Fehler:
+firstName ist vom Typ String
+Erkannte Fehler:
+Alle
+ */
+await instructorRepo.delete({ firstName: 2 });
+
 
 
